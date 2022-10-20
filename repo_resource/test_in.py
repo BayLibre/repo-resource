BayLibre/repo-resource

commit bbb253c421a78ebdc21498790cb91f5349d4c128

common: kill ssh_agent after check/_in operation
If we configure a valid ssh key, followed by an invalid one and we run
check/_in in the same container, we will reuse the ssh_agent credentials
from the previous run.

This is confusing and should not happen.

Kill the ssh_agent() before returning versions/metadata.

Fixes: https://github.com/makohoek/repo-resource/issues/18
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

repo_resource/in_.py
@@ -36,16 +36,26 @@ def in_(instream, dest_dir='.'):
     if config.private_key != '_invalid':
         common.add_private_key_to_agent(config.private_key)
 
-    repo = common.Repo(workdir=Path(dest_dir))
+    try:
+        repo = common.Repo(workdir=Path(dest_dir))
 
-    repo.init(config.url, config.revision, config.name, config.depth)
-    repo.sync(requested_version)
-    fetched_version = repo.currentVersion()
+        repo.init(config.url, config.revision, config.name, config.depth)
+        repo.sync(requested_version)
+        fetched_version = repo.currentVersion()
+    except Exception as e:
+        raise e
+    finally:
+        # always remove the key from the agent
+        if config.private_key != '_invalid':
+            common.remove_private_key_from_agent()
 
     if fetched_version != requested_version:
         raise RuntimeError('Could not fetch requested version')
 
     metadata = repo.metadata()
+
+    if config.private_key != '_invalid':
+        common.remove_private_key_from_agent()
 
     return {"version": {"version": str(fetched_version)},
             "metadata": metadata}

repo_resource/check.py
@@ -36,11 +36,17 @@ def check(instream) -> list:
     if config.private_key != '_invalid':
         common.add_private_key_to_agent(config.private_key)
 
-    repo = common.Repo()
-
-    repo.init(config.url, config.revision, config.name, depth=1)
-    repo.sync()
-    version = repo.currentVersion()
+    try:
+        repo = common.Repo()
+        repo.init(config.url, config.revision, config.name, depth=1)
+        repo.sync()
+        version = repo.currentVersion()
+    except Exception as e:
+        raise e
+    finally:
+        # always remove the key from the agent
+        if config.private_key != '_invalid':
+            common.remove_private_key_from_agent()
 
     new_version = {'version': str(version)}
 

repo_resource/common.py
@@ -6,6 +6,7 @@
 """
 Common functions for Android repo resource
 """
+import atexit
 import logging
 import os
 import sys
@@ -47,6 +48,12 @@ def add_private_key_to_agent(private_key: str):
     finally:
         # always delete the key from the container
         os.unlink(keypath)
+
+
+def remove_private_key_from_agent():
+    ssh_agent_setup._kill_agent()
+    # don't call _kill_agent twice via exit()
+    atexit.unregister(ssh_agent_setup._kill_agent)
 
 
 class SourceConfiguration(NamedTuple):

repo_resource/test_check.py
@@ -198,7 +198,8 @@ class TestCheck(unittest.TestCase):
 
         # This is just a private key randomly generated with
         # ssh-keygen. It should not have access to demo_ssh_manifests_source
-        data['source']['private_key'] = '''-----BEGIN OPENSSH PRIVATE KEY-----
+        data['source']['private_key'] = \
+            '''-----BEGIN OPENSSH PRIVATE KEY-----
 b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
 NhAAAAAwEAAQAAAYEAynCIWzvSq/Tc6qg+/ZHOfTrKoc8h5zeZ/7uGPG/wWVZqp4CHSSae
 w77FCX8gFKAeiE7Dncsw+7WiA7m4gh/Xos8yleSbXnrMJF5nHKpCszuZMz1wjne57WPU1I
@@ -252,7 +253,8 @@ YDbuygyhlR8C8AAAAObWFrb2hvZWtAZ3Jvb3QBAgMEBQ==
 
         # This is just a private key randomly generated with
         # ssh-keygen. It should not have access to demo_ssh_manifests_source
-        data['source']['private_key'] = '''-----BEGIN OPENSSH PRIVATE KEY-----
+        data['source']['private_key'] = \
+            '''-----BEGIN OPENSSH PRIVATE KEY-----
 b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
 NhAAAAAwEAAQAAAYEAynCIWzvSq/Tc6qg+/ZHOfTrKoc8h5zeZ/7uGPG/wWVZqp4CHSSae
 w77FCX8gFKAeiE7Dncsw+7WiA7m4gh/Xos8yleSbXnrMJF5nHKpCszuZMz1wjne57WPU1I

commit cc5f61b39568db23a60a22ba08d82b5d73aa1adb

in: fix duplicate remove_private_key_from_agent call
We should never call remove_private_key_from_agent() twice because it
generates the following python splash:

    in_.in_(instream, str(common.CACHEDIR))
  File "/root/repo_resource/in_.py", line 58, in in_
    common.remove_private_key_from_agent()
  File "/root/repo_resource/common.py", line 54, in remove_private_key_from_agent
    ssh_agent_setup._kill_agent()
  File "/usr/local/lib/python3.10/site-packages/ssh_agent_setup/__init__.py", line 13, in _kill_agent
    del os.environ['SSH_AUTH_SOCK']
  File "/usr/local/lib/python3.10/os.py", line 695, in __delitem__
    raise KeyError(key) from None
KeyError: 'SSH_AUTH_SOCK'

The removal was done properly for the get() but not for the in_()
operation.

Add a smoke test to cover this and fix the duplicate remove call.

Fixes: bbb253c421a7 ("common: kill ssh_agent after check/_in operation")
Reported-by: Guillaume La Roque <glaroque@baylibre.com>
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

repo_resource/in_.py
@@ -54,9 +54,6 @@ def in_(instream, dest_dir='.'):
 
     metadata = repo.metadata()
 
-    if config.private_key != '_invalid':
-        common.remove_private_key_from_agent()
-
     return {"version": {"version": str(fetched_version)},
             "metadata": metadata}
 

repo_resource/test_in.py
@@ -8,9 +8,11 @@ import json
 from io import StringIO
 import shutil
 import unittest
+from pathlib import Path
 
 import repo
 
+from . import check
 from . import common
 from . import in_
 
@@ -23,6 +25,13 @@ class TestIn(unittest.TestCase):
                 'url': 'https://github.com/makohoek/demo-manifests.git',
                 'revision': 'main',
                 'name': 'aosp_device_fixed.xml'
+            }
+        }
+        self.demo_ssh_manifests_source = {
+            'source': {
+                'url': 'https://github.com/makohoek/demo-manifests.git',
+                'revision': 'main',
+                'name': 'baylibre_ssh_project.xml',
             }
         }
 
@@ -75,3 +84,20 @@ class TestIn(unittest.TestCase):
 
         self.assertEquals(result['metadata'][0]['name'], expected_project)
         self.assertEquals(result['metadata'][0]['value'], expected_revision)
+
+    @unittest.skipUnless(
+        Path('development/ssh/test_key').exists(), "requires ssh test key")
+    def test_ssh_private_key(self):
+
+        data = self.demo_ssh_manifests_source
+
+        private_test_key = Path('development/ssh/test_key')
+        data['source']['private_key'] = private_test_key.read_text()
+
+        instream = StringIO(json.dumps(data))
+        versions = check.check(instream)
+
+        data['version'] = versions[0]
+
+        instream = StringIO(json.dumps(data))
+        in_.in_(instream, str(common.CACHEDIR))