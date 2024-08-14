BayLibre/repo-resource

commit c23fc0c2fea11587881c20c55ea55355d0c4901a

Switch to getRevision code based on git ls-remote
Repo versions are computed from manifests with updated revisions for each project.
The repo tool can only generate a manifest with accurate sha1 hashes once all projects
have been downloaded locally (repo manifest -r -o new-manifest.xml).
This uses a lot of network bandwith, memory, cpu and disk space and is a huge waste
of time.
git ls-remote is used instead to fetch revisions much quicker before being injected
into the original manifest. A new variable check_jobs is used to spawn concurrent
processes to make it close to X times faster
Adjust jobs and check_jobs variables based on the git servers capabilities/limitations

Signed-off-by: David Rozé <droze@baylibre.com>

repo_resource/common.py
@@ -275,7 +275,7 @@ class Repo:
         finally:
             self.__restore_oldpwd()
 
-    def sync(self, version: Version = None, jobs: int = 0):
+    def sync(self, version: Version, jobs: int = 0):
         self.__change_to_workdir()
         try:
             with redirect_stdout(sys.stderr):
@@ -288,15 +288,14 @@ class Repo:
                 if jobs > 0:
                     repo_cmd.append('--jobs={}'.format(jobs))
 
-                if version is None:
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    tmp_manifest = os.path.join(tmpdir, 'manifest_tmp')
+                    version.to_file(tmp_manifest)
+                    repo_cmd.append(
+                        '--manifest-name={}'.format(tmp_manifest))
                     repo._Main(repo_cmd)
-                else:
-                    with tempfile.TemporaryDirectory() as tmpdir:
-                        tmp_manifest = os.path.join(tmpdir, 'manifest_tmp')
-                        version.to_file(tmp_manifest)
-                        repo_cmd.append(
-                            '--manifest-name={}'.format(tmp_manifest))
-                        repo._Main(repo_cmd)
+                if os.listdir(self.__workdir) == []:
+                    raise Exception('Sync failed. Is manifest correct?')
         except Exception as e:
             raise (e)
         finally:
@@ -312,17 +311,11 @@ class Repo:
     def save_manifest(self, filename):
         with redirect_stdout(sys.stderr):
             full_path = self.__workdir / filename
-            current_version = self.currentVersion()
             print('Saving manifest to {}'.format(full_path))
-            current_version.to_file(full_path)
+            self.__version.to_file(full_path)
 
     def currentVersion(self) -> Version:
-        with tempfile.TemporaryDirectory() as tmpdir:
-            tmp_manifest = os.path.join(tmpdir, 'manifest_snapshot')
-            self.__manifest_out(tmp_manifest)
-            version = Version.from_file(tmp_manifest)
-
-        return version
+        return self.__version
 
     def metadata(self):
         metadata = []

repo_resource/test_check.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from timeit import default_timer as timer
 import shutil
 import repo
+import xml.etree.ElementTree as ET
 
 from . import check
 from . import common
@@ -113,10 +114,8 @@ class TestCheck(unittest.TestCase):
         }
         instream = StringIO(json.dumps(d))
         check.check(instream)
-        # no assert/assumption to call. repo init and sync should
-        # just be called. maybe we can check for a file as well
-        readme = common.CACHEDIR / 'fetch_artifact' / 'README.md'
-        self.assertTrue(readme.exists())
+        manifests = common.CACHEDIR / '.repo' / 'manifests'
+        self.assertTrue(manifests.exists())
 
     # so here, we init from a public manifest
     # init is completely working fine
@@ -127,7 +126,7 @@ class TestCheck(unittest.TestCase):
         unreachable_projects_data['source']['name'] = 'unreachable_project.xml'
 
         instream = StringIO(json.dumps(unreachable_projects_data))
-        with self.assertRaises(SystemExit):
+        with self.assertRaises(TypeError):
             check.check(instream)
 
     def test_first_revision(self):
@@ -141,7 +140,7 @@ class TestCheck(unittest.TestCase):
         data = self.demo_manifests_source
         data['versions'] = [{
             'version':
-            '<?xml version="1.0" encoding="UTF-8"?>\n<manifest>\n  <remote name="aosp" fetch="https://android.googlesource.com/"/>\n  \n  <default remote="aosp" revision="refs/tags/android-12.0.0_r32"/>\n  \n  <project name="device/generic/common" revision="033d50e2298811d81de7db8cdea63e349a96c9ba" upstream="refs/tags/android-12.0.0_r32" dest-branch="refs/tags/android-12.0.0_r32" groups="pdk"/>\n</manifest>\n'  # noqa: E501
+            '<?xml version="1.0" encoding="UTF-8"?>\n<manifest>\n  <remote name="aosp" fetch="https://android.googlesource.com/"/>\n  \n  <default remote="aosp" revision="refs/tags/android-12.0.0_r32"/>\n  \n  <project name="device/generic/common" path="device/generic/common" revision="033d50e2298811d81de7db8cdea63e349a96c9ba" upstream="refs/tags/android-12.0.0_r32" dest-branch="refs/tags/android-12.0.0_r32" groups="pdk"/>\n</manifest>\n'  # noqa: E501
         }]
         instream = StringIO(json.dumps(data))
         versions = check.check(instream)
@@ -158,7 +157,7 @@ class TestCheck(unittest.TestCase):
         # we passed no version as input, so we should just get current version
         self.assertEqual(len(versions), 1)
         # and we know that version
-        expected_version = '<?xml version="1.0" encoding="UTF-8"?>\n<manifest>\n  <remote name="aosp" fetch="https://android.googlesource.com/"/>\n  \n  <default remote="aosp" revision="refs/tags/android-12.0.0_r32"/>\n  \n  <project name="device/generic/common" revision="033d50e2298811d81de7db8cdea63e349a96c9ba" upstream="refs/tags/android-12.0.0_r32" dest-branch="refs/tags/android-12.0.0_r32" groups="pdk"/>\n</manifest>\n'  # noqa: E501
+        expected_version = '<manifest><remote fetch="https://android.googlesource.com/" name="aosp"></remote><default remote="aosp" revision="refs/tags/android-12.0.0_r32"></default><project groups="pdk" name="device/generic/common" path="device/generic/common" revision="033d50e2298811d81de7db8cdea63e349a96c9ba"></project></manifest>'  # noqa: E501
         version = versions[0]['version']
         self.assertEqual(version, expected_version)
 
@@ -166,13 +165,23 @@ class TestCheck(unittest.TestCase):
     # but we use a newer version (using a different git branch)
     def test_new_revision(self):
         data = self.demo_manifests_source
-        data['versions'] = [{'version': 'older-shasum'}]
+        data['versions'] = [{
+            'version':
+            '<manifest><remote fetch="https://android.googlesource.com/" name="aosp"></remote><default remote="aosp" revision="refs/tags/android-12.0.0_r32"></default><project groups="pdk" name="device/generic/common" revision="older-sha1"></project></manifest>'  # noqa: E501
+        }]
         instream = StringIO(json.dumps(data))
         versions = check.check(instream)
         self.assertEqual(len(versions), 2)
-        expected_version = '<?xml version="1.0" encoding="UTF-8"?>\n<manifest>\n  <remote name="aosp" fetch="https://android.googlesource.com/"/>\n  \n  <default remote="aosp" revision="refs/tags/android-12.0.0_r32"/>\n  \n  <project name="device/generic/common" revision="033d50e2298811d81de7db8cdea63e349a96c9ba" upstream="refs/tags/android-12.0.0_r32" dest-branch="refs/tags/android-12.0.0_r32" groups="pdk"/>\n</manifest>\n'  # noqa: E501
+        expected_version = '<manifest><remote fetch="https://android.googlesource.com/" name="aosp"></remote><default remote="aosp" revision="refs/tags/android-12.0.0_r32"></default><project groups="pdk" name="device/generic/common" path="device/generic/common" revision="033d50e2298811d81de7db8cdea63e349a96c9ba"></project></manifest>'  # noqa: E501
         newest_version = versions[-1]['version']
         self.assertEqual(newest_version, expected_version)
+
+    def test_invalid_revision(self):
+        data = self.demo_manifests_source
+        data['versions'] = [{'version': 'invalid-version'}]
+        instream = StringIO(json.dumps(data))
+        with self.assertRaises(ET.ParseError):
+            check.check(instream)
 
     @unittest.skipUnless(
         Path('development/ssh/test_key').exists(), "requires ssh test key")
@@ -253,7 +262,7 @@ YDbuygyhlR8C8AAAAObWFrb2hvZWtAZ3Jvb3QBAgMEBQ==
 
         instream = StringIO(json.dumps(data))
         versions = []
-        with self.assertRaises(SystemExit):
+        with self.assertRaises(TypeError):
             versions = check.check(instream)
 
         self.assertEqual(len(versions), 0)

repo_resource/check.py
@@ -33,8 +33,15 @@ def check(instream) -> list:
 
     config = common.source_config_from_payload(payload)
 
+    standard_versions = []
+    for v in payload.get('versions', []):
+        standard_versions.append(common.Version(v['version']).standard())
+
     if config.private_key != '_invalid':
         common.add_private_key_to_agent(config.private_key)
+
+    jobs = config.jobs
+    check_jobs = config.check_jobs or jobs*2 or common.DEFAULT_CHECK_JOBS
 
     try:
         repo = common.Repo(config.url,
@@ -42,7 +49,7 @@ def check(instream) -> list:
                            config.name,
                            config.depth)
         repo.init()
-        repo.sync(jobs=config.jobs)
+        repo.update_manifest(jobs=check_jobs)
         version = repo.currentVersion()
     except Exception as e:
         raise e
@@ -51,11 +58,9 @@ def check(instream) -> list:
         if config.private_key != '_invalid':
             common.remove_private_key_from_agent()
 
-    new_version = {'version': str(version)}
-
     versions = payload.get('versions', [])
-    if versions.count(new_version) == 0:
-        versions.append(new_version)
+    if version.standard() not in standard_versions:
+        versions.append({'version': str(version)})
 
     return versions
 

repo_resource/in_.py
@@ -33,6 +33,13 @@ def in_(instream, dest_dir='.'):
     config = common.source_config_from_payload(payload)
     requested_version = common.Version(payload['version']['version'])
 
+    # Check version is valid xml
+    common.Version(payload['version']['version']).standard()
+
+    standard_versions = []
+    for v in payload.get('versions', []):
+        standard_versions.append(common.Version(v['version']).standard())
+
     if config.private_key != '_invalid':
         common.add_private_key_to_agent(config.private_key)
 
@@ -41,7 +48,7 @@ def in_(instream, dest_dir='.'):
                            config.name, config.depth, workdir=Path(dest_dir))
         repo.init()
         repo.sync(requested_version, config.jobs)
-        fetched_version = repo.currentVersion()
+        repo.update_version()
     except Exception as e:
         raise e
     finally:
@@ -49,15 +56,12 @@ def in_(instream, dest_dir='.'):
         if config.private_key != '_invalid':
             common.remove_private_key_from_agent()
 
-    if fetched_version != requested_version:
-        raise RuntimeError('Could not fetch requested version')
-
     # save a copy of the manifest alongside the sources
     repo.save_manifest('.repo_manifest.xml')
 
     metadata = repo.metadata()
 
-    return {"version": {"version": str(fetched_version)},
+    return {"version": {"version": str(requested_version)},
             "metadata": metadata}
 
 

repo_resource/test_in.py
@@ -9,6 +9,7 @@ from io import StringIO
 import shutil
 import unittest
 from pathlib import Path
+import xml.etree.ElementTree as ET
 
 from . import check
 from . import common
@@ -41,7 +42,7 @@ class TestIn(unittest.TestCase):
         data = self.demo_manifests_source
         data['version'] = {'version': 'invalid-version'}
         instream = StringIO(json.dumps(data))
-        with self.assertRaises(repo.error.GitError):
+        with self.assertRaises(ET.ParseError):
             in_.in_(instream, str(common.CACHEDIR))
 
     def test_dest_dir_is_created(self):
@@ -56,6 +57,30 @@ class TestIn(unittest.TestCase):
 
         self.assertTrue(common.CACHEDIR.exists())
 
+    def test_sync_ok(self):
+        data = {
+            'source': {
+                'url': 'https://android.googlesource.com/tools/manifest',
+                'revision': 'fetch_artifact-dev'
+            },
+        }
+        data['version'] = {
+            'version':
+            '<?xml version="1.0" encoding="UTF-8"?>\n<manifest>\n<remote name="aosp" fetch=".." review="https://android-review.googlesource.com/"/>\n<default remote="aosp" revision="master" sync-j="4"/>\n<project name="platform/prebuilts/go/linux-x86" path="prebuilts/go/linux-x86" groups="linux" clone-depth="1"/>\n<project name="tools/fetch_artifact" path="fetch_artifact"/>\n</manifest>'  # noqa: E501
+        }
+        instream = StringIO(json.dumps(data))
+        in_.in_(instream, str(common.CACHEDIR))
+        # no assert/assumption to call. repo init and sync should
+        # just be called. maybe we can check for a file as well
+        readme = common.CACHEDIR / 'fetch_artifact' / 'README.md'
+        self.assertTrue(readme.exists())
+
+    def test_no_manifest_version(self):
+        data = self.demo_manifests_source
+        instream = StringIO(json.dumps(data))
+        with self.assertRaises(KeyError):
+            in_.in_(instream, str(common.CACHEDIR))
+
     def test_valid_in(self):
         data = self.demo_manifests_source
         data['version'] = {
@@ -66,7 +91,10 @@ class TestIn(unittest.TestCase):
         instream = StringIO(json.dumps(data))
         fetched_version = in_.in_(instream, str(common.CACHEDIR))
 
-        self.assertEqual(fetched_version['version'], data['version'])
+        self.assertEqual(
+            common.Version(fetched_version['version']['version']).standard(),
+            common.Version(data['version']['version']).standard()
+        )
 
     def test_get_metadata(self):
         data = self.demo_manifests_source

commit a99841b9a2576bf10d20dfe819544f6087b1d4bc

check: Fix handling of <remove-project /> element
When `repo-resource` creates a version string (which is a full XML
representation of a manifest), the `<remove-project>` tag is not
taken into account.
Because of this, we generate "false positives" for new Versions,
which could trigger unwanted builds.

For example, with manifest [1], we see that the Version string has
the device/amlogic/yukawa project twice, which is wrong.

Handle the <remove-project /> element to fix this.

[1] https://github.com/makohoek/demo-manifests/blob/main/aosp_remove_yukawa_project.xml
Fixes: https://github.com/makohoek/repo-resource/issues/36
Fixes: c23fc0c2fea1 ("Switch to getRevision code based on git ls-remote")
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

repo_resource/common.py
@@ -355,6 +355,7 @@ class Repo:
 
     def update_manifest(self, jobs):
         projects = []
+        removed_projects = []
 
         jobs = jobs or DEFAULT_CHECK_JOBS
         self.__change_to_workdir()
@@ -378,6 +379,24 @@ class Repo:
                     defaultRemote = defaults.get('remote')
                     defaultBranch = defaults.get('revision') \
                         or self.__get_remote_revision(defaultRemote)
+
+                # Handle <remove-project /> element:
+                # - Should not be present in final XML (for version)
+                # - Should remove first matching project by name
+                for p in manifest.findall('remove-project'):
+                    project = p.get('name')
+                    removed_projects.append(project)
+                    manifest.remove(p)
+
+                for p in manifest.findall('project'):
+                    project = p.get('name')
+                    if project in removed_projects:
+                        manifest.remove(p)
+                        removed_projects.remove(project)
+                    # If there are no more projects to remove, we can
+                    # skip parsing the rest of the projects
+                    if not removed_projects:
+                        break
 
                 for p in manifest.findall('project'):
                     project = p.get('name')

repo_resource/test_check.py
@@ -59,6 +59,13 @@ class TestCheck(unittest.TestCase):
                 'name': 'aosp_multiple_device_fixed.xml'
             }
         }
+        self.remove_project_source = {
+            'source': {
+                'url': 'https://github.com/makohoek/demo-manifests.git',
+                'revision': 'main',
+                'name': 'aosp_remove_yukawa_project.xml'
+            }
+        }
 
     def tearDown(self):
         p = common.CACHEDIR
@@ -358,6 +365,26 @@ YDbuygyhlR8C8AAAAObWFrb2hvZWtAZ3Jvb3QBAgMEBQ==
 
         print('fast: {} slow: {}'.format(fast_duration, slow_duration))
         self.assertTrue(fast_duration < slow_duration)
+
+    # test that the `<remove-project>` tag is correctly handled
+    # When rebuilding the Version string.
+    # See: https://github.com/makohoek/repo-resource/issues/36
+    def test_remove_project_version(self):
+        data = self.remove_project_source
+        instream = StringIO(json.dumps(data))
+        versions = check.check(instream)
+
+        expected_version = '<manifest><remote fetch="https://android.googlesource.com/" name="aosp"></remote><remote fetch="https://gitlab.baylibre.com/baylibre/amlogic/atv/aosp/" name="baylibre"></remote><default remote="aosp" revision="refs/tags/android-13.0.0_r62"></default><project name="device/amlogic/yukawa" path="device/amlogic/yukawa" remote="baylibre" revision="0f3423ec0c103d113c2c03953b3a79bb479fd5b7"></project></manifest>'  # noqa: E501
+
+        version = versions[0]['version']
+        self.assertEqual(version, expected_version)
+
+    def test_no_remove_project_element(self):
+        data = self.remove_project_source
+        instream = StringIO(json.dumps(data))
+        versions = check.check(instream)
+        version = versions[0]['version']
+        self.assertNotIn('remove-project', version)
 
 
 if __name__ == '__main__':