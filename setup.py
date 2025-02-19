8695da9a6db8fb82bfb5017334e939e00d8ece2b
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2025-02-19 10:16:51 +0100

    treewide: rename makohoek/mkorpershoek -> baylibre
    Even if I'm the original developer, we should have this
    project hosted as part of our baylibre organization.
    
    Rename everything to make sure it's hosted under the baylibre org.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(name='repo_resource',
       version='0.1',
       description='A concourse resource for the Android repo format',
-      url='https://github.com/makohoek/repo_resource',
+      url='https://github.com/baylibre/repo_resource',
       author='Mattijs Korpershoek',
       author_email='mkorpershoek@baylibre.com',
       license='Apache',
